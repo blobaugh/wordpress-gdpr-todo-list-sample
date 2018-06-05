51f473de781fc5b55a8ef19cd6f7fcf9add07772
Ben Lobaugh <ben@webdevstudios.com>
2018-06-05 18:31:18 +0000

    Initial export version

---
+++ wordpress-gdpr-todo-list-sample.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Plugin Name: GDPR Todo List Sample
+ * Description: Sample project using the WordPress core GDPR functionality.
+ * Version: 1.0.0
+ * Author: Ben Lobaugh, WebDevStudios
+ * Author URI: https://webdevstudios.com
+ */
+
+add_filter( 'wp_privacy_personal_data_exporters', 'register_todo_list_exporter' );
+
+function register_todo_list_exporter( $exporters ) {
+	$exporters['todo-list'] = array(
+		'exporter_friendly_name'	=> __( 'Todo List' ),
+		'callback'					=> 'todo_list_exporter',
+	);
+	return $exporters;
+}
+
+function todo_list_exporter( $email_address, $page = 1 ) {
+	global $wpdb;
+
+	$sql = "SELECT * FROM `todo_list` WHERE email='$email_address'";
+	$list = $wpdb->get_results( $sql );
+
+	$export_items 	= array();
+	$group_id 		= 'todo-items';
+	$group_label	= 'Todo List Items';
+
+	foreach( $list as $item ) {
+
+		$item_data = array(
+			array(
+				'name'	=> __( 'Todo Item' ),
+				'value'	=> $item->todo
+			),
+			array(
+				'name'	=> __( 'Status' ),
+				'value'	=> $item->status,
+			),
+			array(
+				'name'	=> __( 'Created' ),
+				'value'	=> $item->created
+			)
+		);
+
+		$export_items[] = array(
+			'group_id'		=> $group_id,
+			'group_label'	=> $group_label,
+			'item_id'		=> $item->id,
+			'data'			=> array()
+		);
+	}
+
+	return array(
+		'data'	=> $export_items,
+		'done'	=> true
+	);
+}
